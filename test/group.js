1c1995b849a7f6c5d32083c858e9edb2e4239c21
dlyt <miaosonglin@daoxila.com>
2018-09-25 15:46:50 +0800

    组合排序

--- test/group.js
+++ test/group.js
@@ -8,15 +8,34 @@ var result = [];
 doExchange(arr, 0);
 function doExchange(arr, index){
     for (var i = 0; i < arr[index].length; i++) {
-        result[index] = arr[index][i];
-        console.log(result[index])
+        result[index] = arr[index][i]
         if (index != arr.length - 1) {
             // results.push(result.join(','))
             doExchange(arr, index + 1)
         } else {
-            results.push(result.join(','))
+            // console.log(getGroup(result))
+            results.push(result.join('-'))
+            getGroup(result).forEach(item => {
+                results.push(item)
+            });
         }
     }
 }
 
-console.log(results);
+results = new Set(results)
+console.log(results.length)
+ 
+function getGroup(data, index = 0, group = []) {
+	var need_apply = new Array();
+	need_apply.push(data[index]);
+	for(var i = 0; i < group.length; i++) {
+		need_apply.push(group[i] + '-' + data[index]);
+	}
+	group.push.apply(group, need_apply);
+ 
+	if(index + 1 >= data.length) return group;
+	else return getGroup(data, index + 1, group);
+}
+ 
+
+// console.log(getGroup(data));
