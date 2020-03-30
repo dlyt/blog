9d1ab16413403c3820d1d49e5614b29578b56e2a
dlyt <miaosonglin@daoxila.com>
2020-03-30 10:40:51 +0800

    1

--- test/co.js
+++ test/co.js
@@ -1,11 +1,6 @@
-const co = require('co')
-
-co(function*() {
-    setTimeout(function() {
-        console.log(1)
-    }, 100);
-    console.log(2)
-}).catch(err => {
-    
-})
-
+var vkeyWords=/M-/;
+if(vkeyWords.test(`https://event.daoxila.com/M-tools/OneKey2-bdys1/Hotel/?utm_source=Feeds&utm_medium=Baidu&utm_campaign=%E9%97%AA%E6%8A%95-%E6%88%90%E9%83%BD%E5%A9%9A%E5%AE%B4-3.4&city=cd`)){
+    console.log('标题含有js字符串');
+}else{
+    console.log('标题不含有js字符串');
+}
--- test/test.js
+++ test/test.js
@@ -1,18 +1,12 @@
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-// 得到一个两数之间的随机整数，包括两个数在内
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-for (let i = 0; i < 1000; i++) {
-    if (getRandomIntInclusive(1, 20) > 20) {
-        console.log(1)
+const msgArr = [
+    {
+        type: 'beihun',
+        msg: '恭喜您已成功领取￥32000元超全备婚大礼包！微信关注[到喜啦]、回复关键词[礼包] ，即可领取2020年最全备婚清单， 稍后结婚顾问会联系您告知其余优惠的领取方式，下载到喜啦APP：http://dxlfile.com/eFlB，获得更多优惠活动，咨询热线：400-820-1709'
     }
+];
+
+const a = msgArr.find(item => item.type == 'beihun');
+console.log(a)
+if (a) {
+    console.log(1)
 }
